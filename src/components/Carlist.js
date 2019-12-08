6dd0b1875006a749c408f8545dd70c90861972d0
Päivi <paivi@aurora.ad>
2019-12-08 18:42:26 +0200

    Initial commit

---
+++ src/components/Carlist.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css'; 
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar'; /* snackbar */
+import IconButton from '@material-ui/core/IconButton'; /* snackbar */
+import CloseIcon from '@material-ui/icons/Close'; /* snackbar */
+import Addcar from './Addcar';
+import Editcar from './Editcar'
+
+export default function Carlist() {
+  const [cars, setCars] = useState([]);
+  const [open, setOpen] = React.useState(false); /* snackbar */
+
+  useEffect(() => fetchData(), []);
+
+  const fetchData = () => {
+    fetch('https://carstockrest.herokuapp.com/cars')
+    .then(response => response.json())
+    .then(data => setCars(data._embedded.cars))
+    .catch(err => console.error(err))
+  }
+
+  const handleClose = (event, reason) => { /* snackbar */
+    setOpen(false);
+  };
+
+  const deleteCar = (link) => {
+    if (window.confirm('Are you sure')) {
+      fetch(link, {method: 'DELETE'})
+      .then(res => fetchData())
+      .catch(err => console.error(err))
+      setOpen(true); /* snackbar */
+    }
+  }
+
+  const saveCar = (car) => {
+    fetch('https://carstockrest.herokuapp.com/cars', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(car)
+    })
+    .then(res => fetchData())
+    .catch(err => console.error(err))
+  }
+
+  const updateCar = (car, link) => {
+    fetch(link, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(car)      
+    })
+    .then(res => fetchData())
+    .catch(err => console.error(err))
+  }
+
+  const columns = [
+    {
+      Header: 'Brand',
+      accessor: 'brand'
+    },
+    {
+      Header: 'Model',
+      accessor: 'model'
+    },
+    {
+      Header: 'Color',
+      accessor: 'color'
+    },
+    {
+      Header: 'Fuel',
+      accessor: 'fuel'
+    },
+    {
+      Header: 'Year',
+      accessor: 'year'
+    },
+    {
+      Header: 'Price',
+      accessor: 'price'
+    },
+    {
+      sortable: false,
+      filterable: false,
+      width: 100,
+      Cell: row => <Editcar updateCar={updateCar} car={row.original} />
+    },
+    {
+      sortable: false,
+      filterable: false,
+      width: 100,
+      accessor: '_links.self.href',
+      Cell: row => <Button size='small' color='secondary' onClick={() => deleteCar(row.value)}>Delete</Button>
+    }
+  ]
+
+  return (
+    <div>
+      <Addcar saveCar={saveCar} />
+      <ReactTable defaultPageSize={10} filterable={true} data={cars} columns={columns} />
+      <Snackbar
+        anchorOrigin={{vertical: 'bottom', horizontal: 'left',}}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        ContentProps={{'aria-describedby': 'message-id',}}
+        message={<span id="message-id">Car deleted</span>}
+        action={[
+          <IconButton key="close" aria-label="close" color="inherit" onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>,
+        ]}
+        />
+    </div>
+  );
+}
